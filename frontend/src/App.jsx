0589a2c959eca67f4fba4f4ef4ef8e50aceaaa77
harishihan-c <harishihan@outlook.com>
2025-09-02 22:19:40 +0530

    Base frontend folder setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./Pages/HomePage";
+import CreatePage from "./Pages/CreatePage"
+import NoteDetailPage from "./Pages/NoteDetailPage"
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
