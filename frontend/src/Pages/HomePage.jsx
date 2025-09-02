fd16298c71ed196c1ee376238bd8cc91f20555d9
harishihan-c <harishihan@outlook.com>
2025-09-02 22:51:00 +0530

    tailwind css added

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -4,7 +4,12 @@ import toast from "react-hot-toast";
 const HomePage = () => {
   return (
     <div>
-      <button onClick={() => toast.success("success")}>Click me</button>
+      <button
+        onClick={() => toast.success("success")}
+        className="text-red-500 p-4"
+      >
+        Click me
+      </button>
       HomePage
     </div>
   );
