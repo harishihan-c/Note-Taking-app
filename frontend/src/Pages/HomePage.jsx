71c61425b3206efa894ec9fa30ef1db5e4d3d279
harishihan-c <harishihan@outlook.com>
2025-09-15 16:03:36 +0530

    ft: fetching notes from api endpoint

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import toast from "react-hot-toast";
+import NavBar from "../Components/NavBar";
 
 const HomePage = () => {
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch("http://localhost:5001/api/notes");
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Fetching Error", error);
+        toast.error("Cannot load the notes")
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
     <div>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
-      HomePage
+      <NavBar />
     </div>
   );
 };
