9800280199bc8b936d7abbe1a74d6cb06341e0a9
harishihan-c <harishihan@outlook.com>
2025-09-15 16:03:58 +0530

    Cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,23 +2,24 @@ import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/config.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
-
 //Middleware
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use("/api/notes", noteRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-  console.log("Server Started on Port:", PORT);
+    console.log("Server Started on Port:", PORT);
+  });
 });
-});
-
-
